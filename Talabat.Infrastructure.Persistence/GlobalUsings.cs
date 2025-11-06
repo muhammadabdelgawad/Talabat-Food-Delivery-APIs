9fdcc8322e86b6e75afcdc4c35bfb5f3c3fa05bd
Muhammad Abd-Elgawad <muhammad@gamil.com>
2025-11-06 17:02:24 +0200

    Refactor persistence layer and add Order configurations
    Streamlined global usings in `GlobalUsings.cs` to reflect the updated project structure and dependencies.
    
    Updated `Talabat.Infrastructure.Persistence.csproj` to include new folder structures for configurations, migrations, and seed data.
    
    Added `OrederConfigurations` class to define the database schema and relationships for the `Order` entity, including owned properties, property conversions, and relationship constraints.

--- Talabat.Infrastructure.Persistence/GlobalUsings.cs
+++ Talabat.Infrastructure.Persistence/GlobalUsings.cs
@@ -1,9 +1,7 @@
 ﻿global using Microsoft.EntityFrameworkCore;
-global using Talabat.Domain.Common;
-global using Talabat.Domain.Entities;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
-global using Talabat.Infrastructure.Persistence.Data.Configurations.Base;
-global using Talabat.Domain.Contracts;
+global using Talabat.Domain.Common;
+global using Talabat.Domain.Entities.Oreders;
 global using Talabat.Infrastructure.Persistence.Data;
+global using Talabat.Infrastructure.Persistence.Data.Configurations.Base;
 global using Talabat.Infrastructure.Persistence.Repositories;
-global using Talabat.Domain.Contracts.Presistence;
---
+++ Talabat.Infrastructure.Persistence/_Data/Configurations/Products/OrederConfigurations.cs
@@ -0,0 +1,30 @@
+﻿namespace Talabat.Infrastructure.Persistence._Data.Configurations.Products
+{
+    public class OrederConfigurations :BaseEntityConfigurations<Order,int>
+    {
+        public override void Configure(EntityTypeBuilder<Order> builder)
+        {
+            base.Configure(builder);
+            
+            builder.OwnsOne(o => o.ShippingAddress, shippingAddress => shippingAddress.WithOwner());
+
+            builder.Property(Order => Order.Status)
+                .HasConversion(
+                    (status) => status.ToString(),
+                    (status) => (OrderStatus)Enum.Parse(typeof(OrderStatus), status)
+                );
+
+            builder.Property(o => o.OrderDate)
+                .HasColumnType("decimal(8,2)");
+
+            builder.HasOne(o => o.DeliveryMethod)
+                .WithMany()
+                .HasForeignKey(o => o.DeliveryMethodId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            builder.HasMany(o => o.Items)
+                .WithOne()
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
