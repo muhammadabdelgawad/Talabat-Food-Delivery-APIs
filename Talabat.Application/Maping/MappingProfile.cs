6688421ffce93688be6ba44527b383804bcd01e6
Muhammad Abd-Elgawad <muhammad@gamil.com>
2025-11-08 13:13:04 +0200

    Fix typos and add resolvers for mapping improvements
    - Added `IOrderService` interface as a placeholder for future methods.
    - Fixed typos in `ProductPictureUrlResolver` class and its usage.
    - Updated `DependencyInjection` to register the corrected resolver.
    - Added `OrderItemPictureUrlResolver` for resolving `PictureUrl` in `OrderItem` mappings.
    - Introduced `DeliveryMethodDto` to represent delivery method details.
    - Enhanced `MappingProfile` with new mappings for `OrderItem` and fixed existing ones.

--- Talabat.Application/Maping/MappingProfile.cs
+++ Talabat.Application/Maping/MappingProfile.cs
@@ -7,7 +7,7 @@
             CreateMap<Product, ProductToReturnDto>()
                   .ForMember(d => d.Brand, O => O.MapFrom(src => src.Brand!.Name))
                   .ForMember(d => d.Category, O => O.MapFrom(src => src.Category!.Name))
-                  .ForMember(d => d.PictureUrl, O => O.MapFrom<ProductPictureUlrResolver>());   
+                  .ForMember(d => d.PictureUrl, O => O.MapFrom<ProductPictureUrlResolver>());   
             CreateMap<ProductBrand, BrandDto>();
             CreateMap<ProductCategory, CategoryDto>();
 
@@ -19,9 +19,12 @@
             CreateMap<Order, OrderToReturnDto>()
                 .ForMember(dest => dest.DeliveryMethod, options => options.MapFrom(src => src.DeliveryMethod!.ShortName));
 
-
-
+            CreateMap<OrderItem, OrderItemDto>()
+                .ForMember(dest => dest.ProductId, options => options.MapFrom(src => src.Product.Id))
+                .ForMember(dest => dest.ProductName, options => options.MapFrom(src => src.Product.Name))
+                .ForMember(dest => dest.PictureUrl, options => options.MapFrom<OrderItemPictureUrlResolver>());
+            
+            CreateMap<Domain.Entities.Identity.Address, AddressDto>();
         }
     }
 }
- 
