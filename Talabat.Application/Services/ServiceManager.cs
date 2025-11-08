8ac776cd79489aabd2a9bbda7ec1152e654fc1ea
Muhammad Abd-Elgawad <muhammad@gamil.com>
2025-11-08 17:09:16 +0200

    Add support for IOrderService in DI and ServiceManager
    Updated DependencyInjection.cs to register IOrderService with
    a factory method and refactored IBasketService registration.
    Modified ServiceManager to include Lazy<IOrderService>
    initialization and expose it via a new property. Removed unused
    namespaces and added relevant ones to streamline imports.

--- Talabat.Application/Services/ServiceManager.cs
+++ Talabat.Application/Services/ServiceManager.cs
@@ -1,15 +1,17 @@
-﻿namespace Talabat.Application.Services
+﻿
+namespace Talabat.Application.Services
 {
     public class ServiceManager : IServiceManager
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
         private readonly Lazy<IProductService> _productService;
+        private readonly Lazy<IOrderService> _orderService;
         private readonly Lazy<IBasketService> _basketService;
         private readonly IConfiguration _configuration;
         private readonly Lazy<IAuthService> _authService;
         public ServiceManager(IUnitOfWork unitOfWork , IMapper mapper,
-            IConfiguration configuration,Func<IBasketService> basketServiceFactory,
+            IConfiguration configuration,Func<IOrderService> orderServiceFactory,Func<IBasketService> basketServiceFactory,
             Func<IAuthService> authServiceFactory)
         {
             //_configuration=configuration;
@@ -17,6 +19,7 @@
             _mapper=mapper; 
             _productService=new Lazy<IProductService>(()=>new ProductService(_unitOfWork,_mapper));
             _basketService = new Lazy<IBasketService>(basketServiceFactory, LazyThreadSafetyMode.ExecutionAndPublication);
+            _orderService = new Lazy<IOrderService>(orderServiceFactory, LazyThreadSafetyMode.ExecutionAndPublication);
             _authService = new Lazy<IAuthService>(authServiceFactory , LazyThreadSafetyMode.ExecutionAndPublication);
         }
         public IProductService ProductService  =>_productService.Value;
@@ -24,6 +27,8 @@
         public IBasketService BasketService => _basketService.Value;
 
         public IAuthService AuthService => _authService.Value;
+
+        public IOrderService OrderService => _orderService.Value;
     }
 }
  
