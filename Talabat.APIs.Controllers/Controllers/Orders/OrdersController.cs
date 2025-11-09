f7c4de185a109e8e52e4b35a3b095f561ffea231
Muhammad Abd-Elgawad <muhammad@gamil.com>
2025-11-09 13:06:54 +0200

    Add new endpoints and refactor OrdersController
    Refactored the `OrdersController` constructor and `CreateOrder` method for improved readability and consistency.
    Added `GetOrderById` endpoint to retrieve a specific order by ID and `GetDeliveryMethod` endpoint to fetch delivery methods.
    Made minor formatting adjustments for better code clarity.

--- Talabat.APIs.Controllers/Controllers/Orders/OrdersController.cs
+++ Talabat.APIs.Controllers/Controllers/Orders/OrdersController.cs
@@ -3,15 +3,15 @@
 namespace Talabat.APIs.Controllers.Controllers.Orders
 {
     [Authorize]
-    public class OrdersController(IServiceManager serviceManager) :BaseApiController
+    public class OrdersController(IServiceManager serviceManager) : BaseApiController
     {
         [HttpPost]
         public async Task<ActionResult<OrderToReturnDto>> CreateOrder(OrderToCreateDto orderDto)
         {
-            var buyerEmail= User.FindFirstValue(ClaimTypes.Email);
-            
+            var buyerEmail = User.FindFirstValue(ClaimTypes.Email);
+
             var result = await serviceManager.OrderService.CreateOrderAsync(buyerEmail!, orderDto);
-            
+
             return Ok(result);
         }
 
@@ -21,6 +21,23 @@ namespace Talabat.APIs.Controllers.Controllers.Orders
             var buyerEmail = User.FindFirstValue(ClaimTypes.Email);
 
             var result = await serviceManager.OrderService.GetOrdersForUserAsync(buyerEmail!);
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<OrderToReturnDto>> GetOrderById(int id)
+        {
+            var buyerEmail = User.FindFirstValue(ClaimTypes.Email);
+
+            var result = await serviceManager.OrderService.GetOrderByIdAsync(buyerEmail!, id);
+
+            return Ok(result);
+        }
+
+        [HttpGet("delivery")]
+        public async Task<ActionResult<DeliveryMethodDto>> GetDeliveryMethod()
+        {
+            var result = await serviceManager.OrderService.GetDeliveryMethodAsync();
             return Ok(result);
         }
     }
