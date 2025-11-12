17bbaf988168a6614d44e34f53629f4d1cb9ef48
Muhammad Abd-Elgawad <muhammad@gamil.com>
2025-11-12 20:30:48 +0200

    Add caching service with Redis integration
    Introduced a new `ICacheService` interface in the `Talabat.Application.Abstraction.Services.Cache` namespace, defining methods for setting and retrieving cache data.
    
    Implemented the `CacheService` class in the `Talabat.Infrastructure.Cache` namespace, which uses `StackExchange.Redis` for Redis operations and `System.Text.Json` for JSON serialization.
    
    The `SetCacheAsync` method serializes objects to JSON (camelCase) and stores them in Redis with a specified TTL. The `GetCacheAsync` method retrieves cached values by key, returning `null` if the key does not exist.
    
    Dependency injection is used to initialize the `CacheService` with an `IConnectionMultiplexer` instance for Redis database access.

---
+++ Talabat.Infrastructure/Cache/CacheService.cs
@@ -0,0 +1,32 @@
+﻿using StackExchange.Redis;
+using System.Text.Json;
+using Talabat.Application.Abstraction.Services.Cache;
+
+namespace Talabat.Infrastructure.Cache
+{
+    public class CacheService(IConnectionMultiplexer redis) : ICacheService
+    {
+        private readonly IDatabase _database = redis.GetDatabase();
+        
+        public async Task SetCacheAsync(string key, object response, TimeSpan timToLive)
+        {
+            if (response is null) return;
+            
+            var serializedOptions= new JsonSerializerOptions() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase};
+            
+            var serializedResponse = JsonSerializer.Serialize(response, serializedOptions);
+
+            await _database.StringSetAsync(key, serializedResponse, timToLive);
+        }
+
+
+        public async Task<string?> GetCacheAsync(string key)
+        {
+            var reponse = await _database.StringGetAsync(key);
+            if (reponse.IsNull) return null;
+            
+            return reponse;
+        }
+
+    }
+}
